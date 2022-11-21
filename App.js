9d83867a1aa49b5fc57c40ae9897b20465405ba3
Eric Ryu <20jkr@queensu.ca>
2022-11-21 17:02:11 -0500

    new file:   .vscode/settings.json 	modified:   App.js 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import { StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
+import dayjs from "dayjs";
+import customParseFormat from "dayjs/plugin/customParseFormat";
 
-
+dayjs.extend(customParseFormat);
 
 export default function App() {
-  const [location, setLocation] = useState("toronto");
+  const [location, setLocation] = useState("L4G7W7");
   const [error, setError] = useState(null);
   const [lat, setLat] = useState(44);
   const [lon, setLon] = useState(-79);
@@ -18,44 +20,79 @@ export default function App() {
   const OPEN_API = "30374f93c545aa34a2ac616c7d07d46f";
   const GEOCODE_API = "AIzaSyCk4bjICPjyZ5XRCMNMn8xxB4dB88TnUhs";
 
-  const fetchLatLon = async() => {
+  const fetchLatLon = async () => {
     const res = await fetch(
-      `https://maps.googleapis.com/maps/api/geocode/json?`+`address=${location}&key=${GEOCODE_API}`
+      `https://maps.googleapis.com/maps/api/geocode/json?`
+      + `address=${location}&key=${GEOCODE_API}`
     );
-    if (!res.ok){
+    if (!res.ok) {
       throw new Error(`GEOCODE HTTPS ERROR: ${res.status}`);
     }
     const data = await res.json();
     setLat(data.results[0].geometry.location.lat);
     setLon(data.results[0].geometry.location.lng);
-    if (data.results[0].address_components[4].short_name == "US") {
-      setUnit('imperial')
-    }
+    // if (data.results[0].address_components[4].short_name == "US") {
+    //   setUnit('imperial')
+    // }
   };
 
-  const fetchWeather = async() => {
-     const res = await fetch(
+  const fetchWeather = async () => {
+    const res = await fetch(
       `https://api.openweathermap.org/data/2.5/onecall?`
-       + `lat=${lat}&lon=${lon}&exclude={}&appid=${OPEN_API}&units=${unit}`
+      + `lat=${lat}&lon=${lon}&exclude={}&appid=${OPEN_API}&units=${unit}`
     );
-    if (!res.ok){
+    if (!res.ok) {
       throw new Error(`OPENWEATHER HTTPS ERROR: ${res.status}`);
     }
     const data = await res.json();
     setWeather(data);
-    };
-  
+  };
+
+  const onChangeText = (newText) => {
+    setLocation(newText);
+  };
+
+  const LocationSearch = ({
+    location, setLocation,
+    lat, setLat,
+    lon, setLon,
+    fetchLatLon,
+    // fetchWeather
+  }) => {
+
+    // const searchHandle = (e) => {
+    //   fetchLatLon();
+    // };
+
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.weatherSearch}
+          onChangeText={onChangeText}
+          placeholder="Search by City or Postal Code"
+          onSubmitEditing={fetchLatLon()}
+        />
+      </View>
+    )
+  }
+
 
   useEffect(() => {
     fetchWeather()
-  },[lat, lon]);
+  }, [lat, lon]);
 
 
   return (
-  <View style={styles.container}>
-    <Text>{weather.current.temp} C</Text>
-    <StatusBar style="auto" />
-  </View>
+    <View style={styles.container}>
+      <LocationSearch
+        location={location}
+        setLocation={setLocation}
+        lat={lat}
+        setLat={setLat}
+        fetchLatLon={fetchLatLon}
+      />
+      <Text>{weather.current.temp}°</Text>
+      <StatusBar style="auto" />
+    </View>
   );
 };
 
@@ -66,13 +103,27 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  text:{
+  text: {
     textAlign: "center",
   },
-  textTitle:{
+  weatherSearch: {
+    fontSize: 20,
+    textAlign: "center",
+    color: "black",
+    // color: "#FFFFFF",
+    // margin: 12,
+    // marginTop: 12,
+    // borderRadius: 5,
+    // width: "95%",
+    // height: 50,
 
+  },
+  textTitle: {
+    borderRadius: 5,
   },
   buttonContainer: {
     padding: 5,
   },
 });
+
+
