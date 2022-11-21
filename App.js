789336a6c176a8c95a2d144575c42f49ec84480a
Eric Ryu <20jkr@queensu.ca>
2022-11-20 19:43:20 -0500

    modified:   App.js 	modified:   package-lock.json 	modified:   package.json

--- App.js
+++ App.js
@@ -1,58 +1,79 @@
 import { StatusBar } from 'expo-status-bar';
 import React, {useEffect, useState} from "react";
-import dayjs from "dayjs";
-import { StyleSheet, Text, View } from 'react-native';
-import customParseFormat from "dayjs/plugin/customParseFormat";
 import { StyleSheet, Text, View, TextInput, Button, ScrollView } from "react-native";
 
 
 
 export default function App() {
-  const [city, setCity] = useState(null);
-  const [postal, setPostal] = useState(null);
+  const [location, setLocation] = useState("toronto");
   const [error, setError] = useState(null);
+  const [lat, setLat] = useState(44);
+  const [lon, setLon] = useState(-79);
+  const [weather, setWeather] = useState({});
+  const [unit, setUnit] = useState(`metric`);
+  const OPEN_API = "30374f93c545aa34a2ac616c7d07d46f";
+  const GEOCODE_API = "AIzaSyCk4bjICPjyZ5XRCMNMn8xxB4dB88TnUhs";
 
-  const fetchWeather = () => {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/onecall?lat=44&lon=-79&exclude={}&appid=30374f93c545aa34a2ac616c7d07d46f"
-    )
-    .then((res) => res.json())
+  const fetchLatLon = async() => {
+    const res = await fetch(
+      `https://maps.googleapis.com/maps/api/geocode/json?`+`address=${location}&key=${GEOCODE_API}`
+    );
+    if (!res.ok){
+      throw new Error(`GEOCODE HTTPS ERROR: ${res.status}`);
+    }
+    const data = await res.json();
+    setLat(data.results[0].geometry.location.lat);
+    setLon(data.results[0].geometry.location.lng);
+    if (data.results[0].address_components[4].short_name == "US") {
+      setUnit('imperial')
+    }
+  };
 
+  const fetchWeather = async() => {
+     const res = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?`
+       + `lat=${lat}&lon=${lon}&exclude={}&appid=${OPEN_API}&units=${unit}`
+    );
+    if (!res.ok){
+      throw new Error(`OPENWEATHER HTTPS ERROR: ${res.status}`);
+    }
+    const data = await res.json();
+    setWeather(data);
     };
+  
 
-  };
-
-
-  const onPressLocate = () => {
-
-  };
+  useEffect(() => {
+    fetchWeather()
+  },[lat, lon]);
 
 
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <View style={styles.buttonContainer}>
-        <Button
-          title = "Location"
-          onPress={onPressLocate}
-        />
-      </View>
-      <StatusBar style="auto" />
-    </View>
+  <View style={styles.container}>
+    <Text>{JSON.stringify(weather.current.temp)} C</Text>
+    <StatusBar style="auto" />
+  </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#ADD8E6',
     alignItems: 'center',
     justifyContent: 'center',
   },
   text:{
-    textAlign: "center";
+    textAlign: "center",
+  },
+  textTitle:{
+
   },
   buttonContainer: {
     padding: 5,
   },
 });
+
+
+const LocationSearch = ({
+
+})
